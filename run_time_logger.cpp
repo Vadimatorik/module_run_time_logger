97331b482303d613482367e1fd57365409cd0d0e
Vadimatorik <Vadimatorikda@gmail.com>
2018-05-16 23:54:35 +0700

    Логгер стал ассинхронным.

--- run_time_logger.cpp
+++ run_time_logger.cpp
@@ -3,35 +3,87 @@
 #ifdef MODULE_RUN_TIME_LOGGER_ENABLED
 
 #include <stdio.h>
+#include <string.h>
 
-const uint8_t INIT_OK[]					= "[ INIT_OK ]\t\t";
-const uint8_t INIT_ISSUE[]				= "[ INIT_ISSUE ]\t\t";
-const uint8_t INIT_ERROR[]				= "[ INIT_ERROR ]\t\t";
-const uint8_t RUN_MESSAGE_OK[]			= "[ RUN_MESSAGE_OK ]\t";
-const uint8_t RUN_MESSAGE_ISSUE[]		= "[ RUN_MESSAGE_ISSUE ]\t";
-const uint8_t RUN_MESSAGE_ERROR[]		= "[ RUN_MESSAGE_ERROR ]\t";
+const char INIT_OK[]					= "[ INIT_OK ]\t\t";
+const char INIT_ISSUE[]				= "[ INIT_ISSUE ]\t\t";
+const char INIT_ERROR[]				= "[ INIT_ERROR ]\t\t";
+const char RUN_MESSAGE_OK[]			= "[ RUN_MESSAGE_OK ]\t";
+const char RUN_MESSAGE_ISSUE[]		= "[ RUN_MESSAGE_ISSUE ]\t";
+const char RUN_MESSAGE_ERROR[]		= "[ RUN_MESSAGE_ERROR ]\t";
 
-const uint8_t* start_string[6] = {
+const char* startString[6] = {
 	INIT_OK, INIT_ISSUE, INIT_ERROR, RUN_MESSAGE_OK, RUN_MESSAGE_ISSUE, RUN_MESSAGE_ERROR
 };
 
-RunTimeLogger::RunTimeLogger( const run_time_logger_cfg* const cfg ) : cfg( cfg ) {
+RunTimeLogger::RunTimeLogger( const runTimeLoggerCfg* const cfg ) : cfg( cfg ) {
 	this->m = USER_OS_STATIC_MUTEX_CREATE( &mb );
-	this->color[ 0 ] = this->cfg->color.init_ok_color_string;
-	this->color[ 1 ] = this->cfg->color.init_issue_color_string;
-	this->color[ 2 ] = this->cfg->color.init_error_color_string;
-	this->color[ 3 ] = this->cfg->color.run_message_ok_color_string;
-	this->color[ 4 ] = this->cfg->color.run_message_issue_color_string;
-	this->color[ 5 ] = this->cfg->color.run_message_error_color_string;
+	this->color[ 0 ] = this->cfg->color.initOkColorString;
+	this->color[ 1 ] = this->cfg->color.initIssueColorString;
+	this->color[ 2 ] = this->cfg->color.initErrorColorString;
+	this->color[ 3 ] = this->cfg->color.runMessageOkColorString;
+	this->color[ 4 ] = this->cfg->color.runMessageIssueColorString;
+	this->color[ 5 ] = this->cfg->color.runMessageErrorColorString;
+
+	/// В очереди хранятся указатели на строки.
+	this->qMessage	=	USER_OS_STATIC_QUEUE_CREATE( MAX_COUNT_MESSAGE_IN_QUEUE, sizeof( char* ), ( uint8_t* )this->qbMessage, &this->qsMessage );
+
+	/// Создаем задачу, которая будет разбирать запросы.
+	USER_OS_STATIC_TASK_CREATE(	RunTimeLogger::task,
+								"runTimeLogger",
+								RUN_TIME_LOGGER_TASK_SIZE,
+								( void* )this,
+								RUN_TIME_LOGGER_PRIO,
+								this->tbTask,
+								&this->tsTask );
 }
 
-BASE_RESULT RunTimeLogger::sendMessage( RTL_TYPE_M type, const char* string ) {
+void RunTimeLogger::task ( void* pObj ) {
+	RunTimeLogger* o = ( RunTimeLogger* )pObj;
+	char*	message;
+
+	while( true ) {
+		/// Забираем строку для отправки.
+		USER_OS_QUEUE_RECEIVE(o->qMessage, &message, portMAX_DELAY );
+
+		/// Передаем до победного.
+		while( o->cfg->outBuffer( message ) != BASE_RESULT::OK );
+
+		/// Убираем за собой.
+		vPortFree( message );
+	}
+}
+
+
+void RunTimeLogger::sendMessage( RTL_TYPE_M type, const char* string ) {
 	if ( this->m != nullptr)				USER_OS_TAKE_MUTEX( this->m, portMAX_DELAY );
-	BASE_RESULT r;
-	snprintf( this->message_buffer, 512, "%s %s %s \n\r", this->color[ ( uint8_t )type ], start_string[ ( uint8_t )type ], string );
-	r = this->cfg->out_buffer( this->message_buffer );
+
+	uint32_t lenString = 0;
+	lenString += strlen( this->color[ ( uint8_t )type ] ) + 2;
+	lenString += strlen( startString[ ( uint8_t )type ] ) + 2;
+	lenString += strlen( string ) + 2;
+	lenString += 3;
+
+	char* messageString = nullptr;
+
+	/// Ждем, пока появится место в куче.
+	while( true ) {
+		messageString = ( char* )pvPortMalloc( lenString );
+
+		if ( messageString != nullptr )
+			break;
+
+		USER_OS_DELAY_MS( 1 );
+	}
+
+	/// Копируем строчку.
+	snprintf( messageString, lenString, "%s %s %s \n\r", this->color[ ( uint8_t )type ], startString[ ( uint8_t )type ], string );
+
+	USER_OS_QUEUE_SEND( this->qMessage, &messageString, portMAX_DELAY );
+
 	if ( this->m != nullptr)				USER_OS_GIVE_MUTEX( this->m );
-	return r;
+
+	return;
 }
 
 #endif
--- run_time_logger.h
+++ run_time_logger.h
@@ -24,35 +24,49 @@ const char ANSI_BLU[] = "\x1B[34m";
 const char ANSI_MAG[] = "\x1B[35m";
 const char ANSI_CYN[] = "\x1B[36m";
 const char ANSI_WHT[] = "\x1B[37m";
+
 const char ANSI_RESET[] = "\x1B[0m";
 
 // Цвета для разных типов сообщений.
-struct color_message_struct {
-	const char*		init_ok_color_string;
-	const char*		init_issue_color_string;
-	const char*		init_error_color_string;
-	const char*		run_message_ok_color_string;
-	const char*		run_message_issue_color_string;
-	const char*		run_message_error_color_string;
+struct colorMessageStruct {
+	const char*		initOkColorString;
+	const char*		initIssueColorString;
+	const char*		initErrorColorString;
+	const char*		runMessageOkColorString;
+	const char*		runMessageIssueColorString;
+	const char*		runMessageErrorColorString;
 };
 
-struct run_time_logger_cfg {
-	color_message_struct				color;
-	BASE_RESULT ( *out_buffer )			( char* string );
+struct runTimeLoggerCfg {
+	colorMessageStruct					color;
+	BASE_RESULT ( *outBuffer )			( const char* string );
 };
+
+#define MAX_COUNT_MESSAGE_IN_QUEUE				20
+#define RUN_TIME_LOGGER_TASK_SIZE				200
+#define	RUN_TIME_LOGGER_PRIO					2
 
 class RunTimeLogger {
 public:
-	RunTimeLogger( const run_time_logger_cfg* const cfg );
+	RunTimeLogger( const runTimeLoggerCfg* const cfg );
 
-	BASE_RESULT sendMessage( RTL_TYPE_M type, const char* string );
+	void sendMessage( RTL_TYPE_M type, const char* string );
 
 private:
-	mutable	char message_buffer[ 512 ];											// Символов на строку.
-	const run_time_logger_cfg* const cfg;
-	mutable USER_OS_STATIC_MUTEX						m = nullptr;
-	mutable USER_OS_STATIC_MUTEX_BUFFER					mb;
-	mutable	const char*									color[6];
+	static	void	task						( void* pObj );
+
+	const runTimeLoggerCfg* const cfg;
+
+	USER_OS_STATIC_MUTEX						m = nullptr;
+	USER_OS_STATIC_MUTEX_BUFFER					mb;
+	const char*									color[6];
+
+	USER_OS_STATIC_QUEUE						qMessage;
+	USER_OS_STATIC_QUEUE_STRUCT					qsMessage;
+	char*										qbMessage[ MAX_COUNT_MESSAGE_IN_QUEUE ];
+
+	USER_OS_STATIC_STACK_TYPE					tbTask[ RUN_TIME_LOGGER_TASK_SIZE ];
+	USER_OS_STATIC_TASK_STRUCT_TYPE				tsTask;
 };
 
 #endif
